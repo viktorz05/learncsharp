a25bb37ea3305de7c6acc0585941aa30ae11bb90
Viktor Zepeda sanchez <viktor_zepeda_sanchez@sfu.ca>
2025-12-30 01:19:40 -0800

    initial commit

---
+++ module_2/Program.cs
@@ -0,0 +1,45 @@
+﻿Random rand = new();
+int daysUntilExpiration = rand.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0) {
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1) {
+    discountPercentage = 20;
+    Console.WriteLine(@$"Your subscription expires within a day!
+Renew now and save {discountPercentage}%!");
+} else if (daysUntilExpiration <= 5) {
+    discountPercentage = 10;
+    Console.WriteLine($@"Your subscription expires in {daysUntilExpiration} days.
+Renew now and save {discountPercentage}%!");
+} else if (daysUntilExpiration <= 10) {
+    Console.WriteLine(@"Your subscription will expire soon. Renew now!");
+}
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+
+// if (daysUntilExpiration == 0)
+// {
+//     Console.WriteLine("Your subscription has expired.");
+// }
+// else if (daysUntilExpiration == 1)
+// {
+//     Console.WriteLine("Your subscription expires within a day!");
+//     discountPercentage = 20;
+// }
+// else if (daysUntilExpiration <= 5)
+// {
+//     Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+//     discountPercentage = 10;
+// }
+// else if (daysUntilExpiration <= 10)
+// {
+//     Console.WriteLine("Your subscription will expire soon. Renew now!");
+// }
+
+// if (discountPercentage > 0)
+// {
+//     Console.WriteLine($"Renew now and save {discountPercentage}%.");
+// }
